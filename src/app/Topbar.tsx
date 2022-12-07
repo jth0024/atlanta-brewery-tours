549924f0d61cac944f70b366b5125455414172d6
Jordan Hurt <jordanhurt@fullstory.com>
2022-12-06 22:55:31 -0500

    Minor style tweaks

--- src/app/Topbar.tsx
+++ src/app/Topbar.tsx
@@ -1,11 +1,4 @@
-import {
-  Button,
-  ButtonGroup,
-  Flex,
-  Image,
-  Link,
-  Spacer,
-} from '@chakra-ui/react'
+import { Button, Flex, Image, Link, Spacer } from '@chakra-ui/react'
 import NextLink from 'next/link'
 
 const logoSrc = '/images/logo-bug-500-x-500.png'
@@ -14,33 +7,39 @@ export const Topbar = ({ ...rest }) => (
   <Flex
     as="header"
     backgroundColor="white"
-    height="84px"
-    paddingLeft="4"
-    paddingRight="4"
-    paddingTop="0"
-    paddingBottom="0"
-    justifyContent="space-between"
-    alignItems="center"
+    height="96px"
+    px="4"
+    py="2"
+    justifyContent="stretch"
+    alignItems="stretch"
+    overflow="hidden"
     {...rest}
   >
-    <Image
-      objectFit="contain"
-      src={logoSrc}
-      alt="Atlanta Brewery Tours Logo"
-      width="60px"
-      borderRadius="full"
-    />
-    <Spacer display={['block', 'none']} height="2" flex="0 0 auto" />
-    <Flex alignItems="center" marginLeft={[0, 'auto']} as="nav">
-      <Link as={NextLink} href="/about" fontWeight="bold">
-        Blog
-      </Link>
-      <Spacer width={4} />
-      <Link as={NextLink} href="/donate" fontWeight="bold">
-        Tours
-      </Link>
-      <Spacer width={4} />
-      <Button colorScheme="orange">Buy Us a Beer</Button>
+    <Flex
+      flex="1 1 auto"
+      justifyContent="space-between"
+      alignItems="center"
+      overflow="hidden"
+    >
+      <Image
+        objectFit="contain"
+        src={logoSrc}
+        alt="Atlanta Brewery Tours Logo"
+        height="100%"
+        borderRadius="full"
+      />
+      <Spacer display={['block', 'none']} height="2" flex="0 0 auto" />
+      <Flex alignItems="center" marginLeft={[0, 'auto']} as="nav">
+        <Link as={NextLink} href="/about" fontWeight="bold">
+          Blog
+        </Link>
+        <Spacer width={4} />
+        <Link as={NextLink} href="/donate" fontWeight="bold">
+          Tours
+        </Link>
+        <Spacer width={4} />
+        <Button colorScheme="orange">Buy Us a Beer</Button>
+      </Flex>
     </Flex>
   </Flex>
 )
