46e874e7c9322478efcd15254a030726714f643a
Jordan Hurt <jth0024@gmail.com>
2023-10-13 09:06:12 -0400

    Update _document.tsx
    Remove site verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,6 @@ export default function Document() {
   return (
     <Html lang="en-US">
       <Head>
-        <meta
-          name="google-site-verification"
-          content="uddUzvp9WaNXfsQINsDC2Guab4k0ocFO169wf5uD2-4"
-        />
         <link rel="shortcut icon" href="/images/logo-favicon-500-x-500.png" />
       </Head>
       <body>
