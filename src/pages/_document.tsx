04a8a5d7b02411ee91f0f11117967a9e4cb1ba62
Jordan Hurt <jordanhurt@fullstory.com>
2023-12-14 14:11:36 -0500

    Update _document.tsx (#31)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,10 @@ export default function Document() {
   return (
     <Html lang="en-US">
       <Head>
+        <meta 
+            name="google-site-verification"
+            content="uddUzvp9WaNXfsQINsDC2Guab4k0ocFO169wf5uD2-4"
+        />
         <link rel="shortcut icon" href="/images/logo-favicon-500-x-500.png" />
       </Head>
       <body>
