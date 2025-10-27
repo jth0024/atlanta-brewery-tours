d5b5c0edf042c1ff14666520a2a303d701de5a49
Jordan Hurt <jth0024@gmail.com>
2025-10-27 13:47:52 -0400

    Update Logo.tsx

--- src/app/Logo.tsx
+++ src/app/Logo.tsx
@@ -2,13 +2,15 @@ import { Box, Image } from '@chakra-ui/react'
 
 const logoSrc = '/images/logo-bug-500-x-500.png'
 
-export const Logo = ({ ...props }) => (
-  <Box {...props} backgroundColor="background" borderRadius="full">
+export const Logo = ({ ...props, height, width }) => (
+  <Box {...props} backgroundColor="background" borderRadius="full" height={height} width={width}>
     <Image
       objectFit="contain"
       src={logoSrc}
       alt="Atlanta Brewery Tours Logo"
       borderRadius="full"
+      height={height}
+      width={width}
     />
   </Box>
 )
